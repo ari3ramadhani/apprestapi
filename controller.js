3cd8ddcf26335526b210007949d3648b11dc5836
Ari Ramadhani <ari3ramadhani@gmail.com>
2021-03-08 16:11:10 +0700

    berhasil semuadata mhs

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi REST API sudah berjalan",res)
 }
+
+//menampilkan data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error,rows,fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+};
