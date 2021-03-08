5172dc6b5cc43ec47c90b1db80fde0316b61846d
Ari Ramadhani <ari3ramadhani@gmail.com>
2021-03-08 15:35:30 +0700

    test

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API sudah berjalan")
+    response.ok("Aplikasi REST API sudah berjalan",res)
 }
