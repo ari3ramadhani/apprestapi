539197915abd07d2a54707070d77066d27920ba3
Ari Ramadhani <ari3ramadhani@gmail.com>
2021-03-08 19:03:40 +0700

    tambh data

--- controller.js
+++ controller.js
@@ -3,31 +3,46 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok("Aplikasi REST API sudah berjalan",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API sudah berjalan", res)
 }
 
 //menampilkan data mahasiswa
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error,rows,fields){
-        if(error){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
             console.log(error);
-        }else{
-            response.ok(rows,res)
+        } else {
+            response.ok(rows, res)
         }
     });
 };
 
 //menampilkan semua data berdasarkan id
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('Select * From mahasiswa where id_mahasiswa=?', [id],
-    function(error,rows, fields){
-        if(error){
-            console.log(error);
-        }else{
-            response.ok(rows,res)
-        }
-    });
-    
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        });
 }
+//menambah data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan)Values(?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows , fields){
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil menambah data", res)
+            }
+        });
+};
